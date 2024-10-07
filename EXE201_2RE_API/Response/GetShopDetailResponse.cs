59ae691be464353dd539e79f12b41fbc01527b92
1903giabao <baogavip00@gmail.com>
2024-10-07 07:28:05 +0700

    shopDetail

---
+++ EXE201_2RE_API/Response/GetShopDetailResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace EXE201_2RE_API.Response
+{
+    public class GetShopDetailResponse
+    {
+        public string shopName {  get; set; }
+        public string shopLogo { get; set; }
+        public string shopDescription { get; set; }
+        public string shopAddress { get; set; }
+        public int totalRating { get; set; }
+        public int quantityRating { get; set; }
+    }
+}
